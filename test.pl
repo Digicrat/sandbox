6f5ad9e91fbf39e71fcb341e676624eb80363dae
Digicrat <david@empireofgames.com>
2021-01-30 23:05:45 -0500

    Create test.pl

---
+++ test.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use v5.10;
+say "Testing";
+
+=head1 NAME
+
+sample - Using GetOpt::Long and Pod::Usage
+
+=head1 SYNOPSIS
+
+sample [options] [file ...]
+
+ Options:
+   -help            brief help message
+   -man             full documentation
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input file(s) and do something
+useful with the contents thereof.
